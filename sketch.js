1e00e4f0af5cdd0c1ba9b39a2b3e5b778c962f0e
NoNelso <jasonr4nd@gmail.com>
2018-12-20 02:54:21 -0500

    comment updates

--- sketch.js
+++ sketch.js
@@ -39,10 +39,13 @@ function draw() {
 }
 
 function keyPressed() {
+  //MOTION
   if (keyCode === UP_ARROW) s.dir(0, -1);
   else if (keyCode === DOWN_ARROW) s.dir(0, 1);
   else if (keyCode === RIGHT_ARROW) s.dir(1, 0);
   else if (keyCode === LEFT_ARROW) s.dir(-1, 0);
+  //PAUSE AND RESET
   else if (keyCode === ALT) noLoop();
+  else if (keyCode === SHIFT) Loop();
   else if (keyCode === ESCAPE) s.dir(0, 0);
 }
