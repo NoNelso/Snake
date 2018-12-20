51739c6bdda70185ab584e572e6aaf479c0701d1
NoNelso <jasonr4nd@gmail.com>
2018-12-20 01:11:01 -0500

    screwit pull all from thecodingtrain for working moddel

---
+++ food.js
@@ -0,0 +1,14 @@
+// function Food() {
+//   this.x = random(floor(width / scl));
+//   this.y = random(floor(height / scl));
+//   this.generate = function() {
+//     this.x = random(floor(width / scl));
+//     this.y = random(floor(height / scl));
+//   }
+//   this.show = function() {
+//     rect(this.x, this.y, scl, scl);
+//   }
+//   this.update = function(x, y) {
+//     this.generate();
+//   }
+// }
