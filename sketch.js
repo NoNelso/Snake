3ce33e43ded48fa5a82a1c1ee61d57e62f5b1b11
NoNelso <jasonr4nd@gmail.com>
2018-12-19 22:33:59 -0500

    canvas with moving snake

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+var s;
+
 function setup() {
   createCanvas(600, 500);
+  s = new Snake();
 }
 
 function draw() {
+  background(50);
+  s.update();
+  s.show();
 
+}
+
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+
+  this.update = function() {
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  }
+  this.show = function() {
+    rect(this.x, this.y, 10, 10);
+  }
 }
