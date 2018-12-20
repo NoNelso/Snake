388b0466a36ea2b1e5967dd9db7d93bf82c40111
NoNelso <jasonr4nd@gmail.com>
2018-12-19 22:39:59 -0500

    shifted snake to subfile and added controls.

--- sketch.js
+++ sketch.js
@@ -12,17 +12,9 @@ function draw() {
 
 }
 
-function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 1;
-  this.yspeed = 0;
-
-  this.update = function() {
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
-  }
-  this.show = function() {
-    rect(this.x, this.y, 10, 10);
-  }
+function keyPressed() {
+  if (keyCode === UP_ARROW) s.dir(0, -1);
+  else if (keyCode === DOWN_ARROW) s.dir(0, 1);
+  else if (keyCode === RIGHT_ARROW) s.dir(1, 0);
+  else if (keyCode === LEFT_ARROW) s.dir(-1, 0);
 }
